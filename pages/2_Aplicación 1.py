f99139ad5ca5b925d798d0daf0dd4c87f2034b4d
jose <joseospinahiguita4474@gmail.com>
2023-10-07 15:48:24 -0500

    c1

--- pages/2_Aplicación 1.py
+++ pages/2_Aplicación 1.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import webcolors 
 
-# Diseño personalizado
-st.header("Aplicacón 1")
+st.title("Buscador de Colores")
+
+rgb_value = st.text_input("Introduce el valor RGB (ejemplo: 255, 0, 0):")
+
+if rgb_value:
+    try:
+        
+        rgb_values = tuple(map(int, rgb_value.split(',')))
+
+        
+        color_name = webcolors.rgb_to_name(rgb_values)
+
+       
+        st.write(f"Nombre del color: {color_name}")
+    except ValueError:
+        st.error("Entrada no válida. Asegúrate de usar el formato correcto (ejemplo: 255, 0, 0).")
+
+
+st.sidebar.markdown("Hecho por tu nombre")
